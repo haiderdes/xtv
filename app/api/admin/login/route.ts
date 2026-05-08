a744272226a318142d619324862d58ecf0d3d12b
haidernot <haidar.designer@gmail.com>
2026-05-08 20:50:08 +0300

    change admin password

--- app/api/admin/login/route.ts
+++ app/api/admin/login/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 import { cookies } from "next/headers"
 
 const ADMIN_USERNAME = process.env.ADMIN_USERNAME || "admin"
-const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "admin123"
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "Ha!der89hxsat$e"
 
 export async function POST(request: Request) {
   try {
